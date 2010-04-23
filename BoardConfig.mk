ade6ba91476c63df0af98afd7a406a518329b3b5
Steve Kondik <shade@chemlab.org>
2010-04-23 09:57:44 -0400

    Build media HAL from source.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,8 @@ BOARD_USES_GENERIC_AUDIO := false
 BOARD_USE_HTC_LIBSENSORS := true
 BOARD_HTC_LIBSENSORS_NAME := sensors.msm7k
 
+BOARD_USES_QCOM_LIBS := true
+
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 msmsdcc_sdioirq=1 wire.search_count=5
 
 BOARD_HAVE_BLUETOOTH := true
